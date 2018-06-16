3420976bccaa185c15001a6f6dab2765d02d398d
BuildTools <unconfigured@null.spigotmc.org>
2018-06-16 14:07:31 +0200

    Scraper: récupération du body de chaque classes

--- scraper.js
+++ scraper.js
@@ -1,12 +1,31 @@
 var request = require("request");
 var cheerio = require("cheerio");
 
-
-console.log(list.html());
-
-request({
-  uri: "http://www.sitepoint.com",
-}, function(error, response, body) {
-  console.log(body);
-  var $ = cheerio.load(body);
-});
+dofusIdClasses = [
+  '6-ecaflip',
+  '7-eniripsa',
+  '8-iop',
+  '9-cra',
+  '1-feca',
+  '11-sacrieur',
+  '10-sadida',
+  '2-osamodas',
+  '3-enutrof',
+  '4-sram',
+  '5-xelor',
+  '12-pandawa',
+  '13-roublard',
+  '14-zobal',
+  '15-steamer',
+  '16-eliotrope',
+  '17-huppermage',
+  '18-ouginak'
+];
+for (i in dofusIdClasses) {
+  request({
+    uri: "https://www.dofus.com/fr/mmorpg/encyclopedie/classes/"+i,
+  }, function(error, response, body) {
+    console.log(body);
+    var $ = cheerio.load(body);
+  });
+};
